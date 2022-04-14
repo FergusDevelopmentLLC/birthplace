607dd81b489ce4710a13cf450822f96ec198be8e
EC2 Default User <ec2-user@ip-172-31-41-150.ec2.internal>
2022-04-14 20:49:03 +0000

    microservice working

---
+++ famous-api/handler.js
@@ -0,0 +1,146 @@
+'use strict';
+const dbConfig = require('./db')
+
+const { Client } = require('pg')
+
+const getGeoJsonSqlFor = (sql) => {
+  
+  //remove trailing ; if present
+  if(sql.charAt(sql.length - 1) === ';') sql = sql.substr(0, sql.length - 1)
+  
+  return `SELECT jsonb_build_object(
+            'type', 'FeatureCollection',
+            'features', jsonb_agg(features.feature)
+          )
+          FROM 
+          (
+            SELECT jsonb_build_object(
+            'type', 'Feature',
+            'geometry', ST_AsGeoJSON(shape,3)::jsonb,
+            'properties', to_jsonb(inputs) - 'shape'
+          ) AS feature
+          FROM 
+            (
+              ${sql}
+            ) inputs
+          ) features;`
+}
+
+module.exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Hooray! It works!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.getCountries = (event, context, callback) => {
+
+  const client = new Client(dbConfig)
+  client.connect()
+
+  let sql = `SELECT 
+    id, 
+    name, 
+    country_code, 
+    shape
+  FROM public.countries
+  LIMIT 10;`.trim()
+
+  sql = getGeoJsonSqlFor(sql)
+
+  client
+    .query(sql, null)
+    .then((res) => {
+      
+      const response = {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": '*',
+          "Access-Control-Allow-Methods": 'GET'
+        },
+        body: JSON.stringify(res.rows[0]['jsonb_build_object']),
+      }
+
+      callback(null, response)
+
+      client.end()
+    })
+    .catch((error) => {
+
+      const errorResponse = {
+        statusCode: error.statusCode || 500,
+        body: `${error}`,
+      }
+
+      callback(null, errorResponse)
+
+      client.end()
+    })
+}
+
+module.exports.getFamousWhere = (event, context, callback) => {
+
+  let sql = 
+  `SELECT 
+    id,
+    name,
+    domain,
+    industry,
+    occupation,
+    gender,
+    birth_year,
+    birth_place,
+    bp_lat,
+    bp_lon,
+    country_code,
+    st_setsrid(st_makepoint(bp_lon, bp_lat), 4326) as shape
+  FROM famous where {}`.trim()
+
+  sql = sql.replace("{}", event.pathParameters.clause.replace('"','').replace('"',''));
+
+  console.log(sql)
+
+  sql = getGeoJsonSqlFor(sql)
+
+  const client = new Client(dbConfig)
+  client.connect()
+
+  client
+    .query(sql, null)
+    .then((res) => {
+      
+      const response = {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": '*',
+          "Access-Control-Allow-Methods": 'GET'
+        },
+        body: JSON.stringify(res.rows[0]['jsonb_build_object']),
+      }
+
+      callback(null, response)
+
+      client.end()
+    })
+    .catch((error) => {
+
+      const errorResponse = {
+        statusCode: error.statusCode || 500,
+        body: `${error}`,
+      }
+
+      callback(null, errorResponse)
+
+      client.end()
+    })
+}
